a233a1ac80d3506249ce107e9ed5796d96d541d2
characteress <gustavogcm.ferragil@gmail.com>
2018-12-05 08:19:56 -0200

    Revert "Add Arch Power and Gentle Fair Sleepers Interface"
    This reverts commit 0583659d45930158282046d0a808fb3f6c7b43ec.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4755,13 +4755,6 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
-static unsigned int Larch_power = 0;
-
-void relay_ap(unsigned int ap)
-{
-	Larch_power = ap;
-}
-
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -7963,7 +7956,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = SCHED_CAPACITY_SCALE;
 	struct sched_group *sdg = sd->groups;
 
-
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_cpu_capacity(sd, cpu);
 	else
@@ -7972,7 +7964,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
 	sdg->sgc->capacity_orig = capacity;
-
 
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_freq_capacity(sd, cpu);
